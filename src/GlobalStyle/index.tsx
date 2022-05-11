a8ebc04c8f8e4af9e3cd2e4d56e5ec71f2817b44
Nix <nix6839@washnix.com>
2022-05-11 22:02:22 +0900

    refactor: remove ResetStyle
    Emotion already include reset css.

--- src/GlobalStyle/index.tsx
+++ src/GlobalStyle/index.tsx
@@ -1,7 +1,6 @@
 import { Global } from '@emotion/react';
 import PretendardFont from './PretendardFont';
-import ResetStyle from './ResetStyle';
 import Theme from './Theme';
 import ZIndex from './ZIndex';
 
-export default <Global styles={[Theme, ZIndex, PretendardFont, ResetStyle]} />;
+export default <Global styles={[Theme, ZIndex, PretendardFont]} />;
