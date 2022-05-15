950c1ed278f36a2666cfb73f88690d1d9f3eefb5
Nix <nix6839@washnix.com>
2022-05-15 12:39:35 +0900

    refactor: seperate theme sections

---
+++ src/theme/fonts.ts
@@ -0,0 +1,9 @@
+const cssFont =
+  "Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, 'Helvetica Neue', 'Segoe UI', 'Apple SD Gothic Neo', 'Noto Sans KR', 'Malgun Gothic', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', sans-serif";
+
+const fonts = {
+  heading: cssFont,
+  body: cssFont,
+};
+
+export default fonts;
---
+++ src/theme/index.ts
@@ -0,0 +1,10 @@
+import { extendTheme } from '@chakra-ui/react';
+import colors from './colors';
+import fonts from './fonts';
+import styles from './styles';
+
+export default extendTheme({
+  colors,
+  fonts,
+  styles,
+});
